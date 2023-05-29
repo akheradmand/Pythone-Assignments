d97f84046a393dd15456c2ca15e1e3e541ee62fc
akheradmand <akhradmand2002@yahoo.com>
2023-05-30 01:59:39 +0330

    check function in ass23

--- assignment23/main.py
+++ assignment23/main.py
@@ -17,9 +17,14 @@ class MainWindow(QMainWindow):
             for j in range(9):
                 new_cell=QLineEdit()
                 self.ui.grid_layout.addWidget(new_cell,i,j)
-                new_cell.textChanged.connect(partial(self.validation,i,j))
+                # new_cell.textChanged.connect(partial(self.validation,i,j))
                 self.line_edits[i][j]=new_cell
         self.new_game()
+        for i in range(0,9):
+            for j in range(0,9):
+                self.line_edits[i][j].textChanged.connect(partial(self.validation,i,j))
+
+        
 
     def open_file(self):
         file_path=QFileDialog.getOpenFileName(self,"Open File...")[0]
@@ -49,28 +54,79 @@ class MainWindow(QMainWindow):
         for i in range(9):
             for j in range(9):
                 self.line_edits[i][j].setReadOnly(False)
+                # self.line_edits[i][j].setStyleSheet("background-color:white")
                 if puzzle.board[i][j] != None:
                     self.line_edits[i][j].setText(str(puzzle.board[i][j]))
                     self.line_edits[i][j].setReadOnly(True)
+                    # self.line_edits[i][j].setStyleSheet("background-color:gray")
                 else:
                     self.line_edits[i][j].setText("")
 
-    def check(self):
-        for i in range(0,9):
-            for j in range(0,9):
-                number1=self.line_edits[i][0].text()
-                number2=self.line_edits[i][j].text()
-                if number1==number2:
+    def check(self,i,j):
+        for m in range(0,9):
+            for n in range(0,9):
+                self.line_edits[m][n].setStyleSheet("background-color:white")
+        
+        number1=self.line_edits[i][j].text()
+        for k in range(0,9):
+            number2=self.line_edits[i][k].text()
+            if j != k and number2 != "" and number1==number2:
+                self.line_edits[i][j].setStyleSheet("background-color:red")
+                self.line_edits[i][k].setStyleSheet("background-color:blue")
+                print("***")
+                return False
+            
+        for k in range(0,9):
+            number2=self.line_edits[k][j].text()
+            if i != k and number2 != "" and number1==number2:
+                self.line_edits[i][j].setStyleSheet("background-color:red")
+                self.line_edits[k][j].setStyleSheet("background-color:blue")
+                print("***")
+                return False
+                
+        if 0<=i<3:
+            if 0<=j<3:
+                self.square_check(i,j,0,0,number1)
+            elif 3<=j<6:
+                self.square_check(i,j,0,3,number1)
+            elif 6<=j<9:
+                self.square_check(i,j,0,6,number1)
+        elif 3<=i<6:
+            if 0<=j<3:
+                self.square_check(i,j,3,0,number1)
+            elif 3<=j<6:
+                self.square_check(i,j,3,3,number1)
+            elif 6<=j<9:
+                self.square_check(i,j,3,6,number1)
+        elif 6<=i<9:
+            if 0<=j<3:
+                self.square_check(i,j,6,0,number1)
+            elif 3<=j<6:
+                self.square_check(i,j,6,3,number1)
+            elif 6<=j<9:
+                self.square_check(i,j,6,6,number1)
+
+    def square_check(self,i,j,lower_row_bound,lower_col_bound,number1):
+        for m in range(lower_row_bound,3+lower_row_bound):
+            for n in range(lower_col_bound,3+lower_col_bound):
+                number2=self.line_edits[m][n].text()
+                if m!=i and n!=j and number2 != "" and number1==number2:
+                    self.line_edits[i][j].setStyleSheet("background-color:red")
+                    self.line_edits[m][n].setStyleSheet("background-color:blue")
                     print("***")
                     return False
-                
+
     def validation(self,i,j,text):
         if text not in ["1","2","3","4","5","6","7","8","9"]:
             self.line_edits[i][j].setText("")
 
-        self.check()
+        self.check(i,j)
 
-        if self.check()==True:
+        # if self.check(i,j)==False and self.line_edits[i][j].text()=="":
+        #     self.line_edits[i][j].setStyleSheet("background-color:green")
+        #     self.check(i,j)
+
+        if self.check(i,j)==True:
             msg_box=...
 
 if __name__=="__main__":
